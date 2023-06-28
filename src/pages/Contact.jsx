4cd20adf078053df138747ed3ee75bde0ad4711e
iamalexstanciu <alexstanciu@outlook.it>
2023-06-28 18:33:47 +0300

    worked on main page and contact form

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "../styles/contact.css";
+
+function Contact() {
+  return (
+    <div>
+      <form action="https://formsubmit.co/Misterale22@gmail.com" method="POST">
+        <input type="text" name="name" required placeholder="Your name" />
+        <input type="email" name="email" required placeholder="Email address" />
+        <textarea name="message" placeholder="Details of your problem" />
+        <input type="file" name="attachment" accept="image/png, image/jpeg" />
+        <input
+          type="hidden"
+          name="_autoresponse"
+          value="Thanks for the message, i will answer as soon as possible! Have a great day! Alex"
+        />
+
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  );
+}
+
+export default Contact;
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,16 +1,35 @@
 import React from "react";
-import Navbar from "../components/navbar.jsx";
 import "../styles/homepage.css";
+import Logo from "../assets/logo (2).png";
+import Soon from "../assets/soon.png";
+import Contact from "./Contact";
 
 function Homepage() {
   return (
     <>
-      <Navbar />
       <div className="home-container">
-        <div className="about">Talking about the application.</div>
-        <div className="futures">Talking about what will come.</div>
-        <div className="download">Link to donwload the app from PlayStore.</div>
+        <div className="about">
+          <h2>DelivMap is the application for couriers.</h2>
+          <p>
+            The main point of the application is to help couriers who have to
+            organize their delivery day in a fast, simple way and avoid wasting
+            time along the way.
+          </p>
+        </div>
+        <div className="futures">
+          <h2>Next</h2>
+          <p>
+            v1.0 - brings an MVP that you can test and actually understand the
+            idea and implementation, also looking for opinions and ideas for
+            future version.
+          </p>
+        </div>
+        <div className="download">
+          <img src={Logo} alt="application logo" />
+          <img src={Soon} alt="soon download link" />
+        </div>
       </div>
+      <Contact/>
     </>
   );
 }
